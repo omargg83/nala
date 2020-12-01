93e56e39aed3661bfaa1cfa2192a34cea2514ee7
Omar <51249360+omargg83@users.noreply.github.com>
2020-11-30 19:44:45 -0600

    a
    a

--- a_reporte/reporte2.php
+++ a_reporte/reporte2.php
@@ -16,17 +16,17 @@
     <div class="alert alert-light" role="alert">
       <h4 class="alert-heading">VENTAS POR VENDEDOR</h4>
       <div class='row'>
-        <div class='col-sm-3'>
+        <div class='col-4'>
             <label><b>Del</b></label>
             <input class="form-control fechaclass" placeholder="Desde...." type="date" id='desde' name='desde' value='<?php echo $fecha1; ?>' autocomplete="off">
         </div>
 
-        <div class='col-sm-3'>
+        <div class='col-4'>
           <label><b>Al</b></label>
           <input class="form-control fechaclass" placeholder="Hasta...." type="date" id='hasta' name='hasta' value='<?php echo $fecha; ?>' autocomplete="off">
         </div>
 
-        <div class='col-sm-3'>
+        <div class='col-4'>
           <label><b>Vendedor</b></label>
           <select class="form-control" name="idusuario" id="idusuario">
             <option value=""></option>
@@ -42,9 +42,11 @@
       </div>
       <hr>
       <div class='row'>
-        <div class='col-sm-4'>
+        <div class='col-12'>
           <div class='btn-group'>
-            <button title='Buscar' class='btn btn-outline-warning btn-sm' id='buscar_canalizado' type='submit' id='lista_buscar'><i class='fa fa-search'></i><span> Buscar</span></button>
+            <button title='Buscar' class='btn btn-warning btn-sm' id='buscar_canalizado' type='submit' id='lista_buscar'><i class='fa fa-search'></i><span> Buscar</span></button>
+
+            <button type='button' class='btn btn-warning btn-sm' id='lista_cat' is='b-link'  des='a_reporte/index' dix='contenido' title='regresar'><i class='fas fa-undo-alt'></i>Regresar</button>
           </div>
         </div>
       </div>
--- a_reporte/reporte4.php
+++ a_reporte/reporte4.php
@@ -16,17 +16,17 @@
     <div class="alert alert-light" role="alert">
       <h4 class="alert-heading">CORTE DE CAJA POR USUARIO</h4>
       <div class='row'>
-        <div class='col-sm-3'>
+        <div class='col-4'>
             <label><b>Del</b></label>
             <input class="form-control fechaclass" placeholder="Desde...." type="date" id='desde' name='desde' value='<?php echo $fecha1; ?>' autocomplete="off">
         </div>
 
-        <div class='col-sm-3'>
+        <div class='col-4'>
           <label><b>Al</b></label>
           <input class="form-control fechaclass" placeholder="Hasta...." type="date" id='hasta' name='hasta' value='<?php echo $fecha; ?>' autocomplete="off">
         </div>
 
-        <div class='col-sm-3'>
+        <div class='col-4'>
           <label><b>Vendedor</b></label>
           <select class="form-control" name="idusuario" id="idusuario" required>
             <option value=""></option>
@@ -41,11 +41,13 @@
       </div>
       <hr>
       <div class='row'>
-        <div class='col-sm-4'>
+        <div class='col-12'>
           <div class='btn-group'>
-            <button title='Buscar' class='btn btn-outline-warning btn-sm' id='buscar_canalizado' type='submit' id='lista_buscar'><i class='fa fa-search'></i><span> Consultar</span></button>
+            <button title='Buscar' class='btn btn-warning btn-sm' id='buscar_canalizado' type='submit' id='lista_buscar'><i class='fa fa-search'></i><span> Consultar</span></button>
 
-            <button type='button' class='btn btn-warning btn-sm mr-2'  id='print_persona' is='f-print' title='Editar' des='a_reporte/cortexusr' dix='resultado'><i class='fas fa-print'></i>Imprimir</button>
+            <button type='button' class='btn btn-warning btn-sm'  id='print_persona' is='f-print' title='Editar' des='a_reporte/cortexusr' dix='resultado'><i class='fas fa-print'></i>Imprimir</button>
+
+            <button type='button' class='btn btn-warning btn-sm' id='lista_cat' is='b-link'  des='a_reporte/index' dix='contenido' title='regresar'><i class='fas fa-undo-alt'></i>Regresar</button>
 
           </div>
         </div>
