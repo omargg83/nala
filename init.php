d5e26414d0fe9ea2787aff7d95f55f1f4de521a5
omargg83 <51249360+omargg83@users.noreply.github.com>
2020-12-03 09:22:33 -0600

    a
    a

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 $server=2;
-$_SESSION['des']=0;
+$_SESSION['des']=1;
 $_SESSION['pagina']=50;
 
 if($server==2){
@@ -18,13 +18,5 @@ else if($server==3){
   define("SERVIDOR", "sagyc.com.mx");
   define("BDD", "sagycrmr_nala");
   define("PORT", "3306");
-}
-else if($server==4){
-  //////////localhost 2
-  define("MYSQLUSER", "root");
-  define("MYSQLPASS", "root");
-  define("SERVIDOR", "localhost");
-  define("BDD", "sagycrmr_nala");
-  define("PORT", "8889");
 }
 ?>
